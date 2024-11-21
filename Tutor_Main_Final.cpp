b4c634cececaa164bd4ee68df633eb62f6abbef9
Andrew Toderian <atoderian@outlook.com>
2024-11-21 09:21:32 -0800

    add files

---
+++ Tutor_Main_Final.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Owner on 11/21/2024.
+//
+
+#include "Tutor_Header_Final.h"
+int main(){
+    add_tutor("Andrew","Toderian","Science","50.00",
+      "Kamloops","Canada","test","this is my bio it is very long and a good way to test this");
+    return 0;
+}
