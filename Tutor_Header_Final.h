2ef65c1c186e683e580212cfd54760c8ea55af0d
Harjot S. <92822546+Inthydra@users.noreply.github.com>
2024-11-26 17:46:35 -0800

    Update Tutor_Header_Final.h
    No longer using namespace std since its causing issues

--- Tutor_Header_Final.h
+++ Tutor_Header_Final.h
@@ -6,43 +6,41 @@
 #include <vector>
 #include <fstream>
 #include <queue>
-#include <math.h>
+#include <cmath>
 #include <list>
 #include <sstream>
 
-
-using namespace std;
-
 // File names to use in functions
-extern string filename1; // ListOfTutors_F.txt
-extern string filename2; // KeyList_F.txt
+extern std::string filename1; // ListOfTutors_F.txt
+extern std::string filename2; // KeyList_F.txt
 
 // Function Declarations
 
 // Adds a tutor to both the main data file and the key file
-void add_tutor(const string& fname, const string& lname, const string& subject,
-    const string& price, const string& city, const string& country,
-    const string& email, const string& bio);
+void add_tutor(const std::string& fname, const std::string& lname, const std::string& subject,
+    const std::string& price, const std::string& city, const std::string& country,
+    const std::string& email, const std::string& bio);
 
 // Converts a string from a file to a vector using "|" as a delimiter
-vector<string> line_to_vector(const string& line);
+std::vector<std::string> line_to_vector(const std::string& line);
 
-list<vector<string>> listSort(list<vector<string>> input_list, vector<string> input_vec);
+std::list<std::vector<std::string>> listSort(std::list<std::vector<std::string>> input_list, std::vector<std::string> input_vec);
 
-queue<vector<string>> sortQ(queue<vector<string>> input);
+std::queue<std::vector<std::string>> sortQ(std::queue<std::vector<std::string>> input);
 
-string joinTutorLine(const vector<string>& tutorDetails);
+std::string joinTutorLine(const std::vector<std::string>& tutorDetails);
+
 // Searches the key file for a key and adds it to a queue
-queue<vector<string>> SearchKeyFile(queue<vector<string>> Results, string key1);
+std::queue<std::vector<std::string>> SearchKeyFile(std::queue<std::vector<std::string>> Results, std::string key1);
 
 // Prints a vector to the terminal
-void printV(vector<string> input);
+void printV(std::vector<std::string> input);
 
 // Prints a queue of vectors to the terminal
-void printQ(queue<vector<string>> input);
+void printQ(std::queue<std::vector<std::string>> input);
 
 // Gets a tutor's line from the main data file based on the location
-vector<string> GetTutorLine(double location);
+std::vector<std::string> GetTutorLine(double location);
 
 // Gets the rating or the number of ratings from a line in the main data file
 double getRating(double location, bool R_or_num);
@@ -53,4 +51,4 @@ void insert_New_Rating(double location, double newRating);
 // Rates a tutor by calculating the new rating and number of ratings, and then updates the files
 void Rate_Tutor(double location, double new_rate);
 
-#endif 
+#endif
