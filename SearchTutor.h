1b7b8c801545b4373015fe11b6a3502955f30903
Harjot S. <92822546+Inthydra@users.noreply.github.com>
2024-11-26 17:04:35 -0800

    Update SearchTutor.h
    Update to header file to declare functions

--- SearchTutor.h
+++ SearchTutor.h
@@ -7,6 +7,10 @@
 class SearchTutor : public wxDialog {
 public:
     SearchTutor(wxWindow* parent);
+    void DisplayTutorDetails(wxListBox* tutorList, int selectedIndex, wxChoice* filterDropdown);
+    void RateTutor(wxListBox* tutorList, int selectedIndex, wxChoice* filterDropdown);
+    void PopulateFilteredTutors(wxListBox* tutorList, const std::string& subject);
+    void PopulateAllTutors(wxListBox* tutorList);
 };
 
 #endif 
